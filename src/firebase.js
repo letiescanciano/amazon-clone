2a23bf266fe10dfb223cf9c1bca928faed4e12b7
letiescanciano <leticia.escanciano@gmail.com>
2020-09-09 12:00:02 +0200

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD1WSLIj1K4tdD-XLsFWXUDPoGIkKhbfH8",
+  authDomain: "clone-29956.firebaseapp.com",
+  databaseURL: "https://clone-29956.firebaseio.com",
+  projectId: "clone-29956",
+  storageBucket: "clone-29956.appspot.com",
+  messagingSenderId: "289974530327",
+  appId: "1:289974530327:web:6027e682c4d37af3ce8752",
+  measurementId: "G-ECSWDWX4FT",
+};
